128d9126a0fd5bb55fa14f939146a1585efeeb22
JarekSz <jszymczyk@comcast.net>
2021-04-08 17:18:22 -0500

    Initial Commit

---
+++ MyTwitterSwiftUI/MyTwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTwitterSwiftUIApp.swift
+//  MyTwitterSwiftUI
+//
+//  Created by Jaroslaw Szymczyk iMac on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
