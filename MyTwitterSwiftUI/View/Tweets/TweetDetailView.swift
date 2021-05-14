538504e4775fbeca04a2de940748048ae8e0dff6
JarekSz <jszymczyk@comcast.net>
2021-05-13 22:35:07 -0500

    Check if user likes tweet.

--- MyTwitterSwiftUI/View/Tweets/TweetDetailView.swift
+++ MyTwitterSwiftUI/View/Tweets/TweetDetailView.swift
@@ -10,62 +10,73 @@ import Kingfisher
 
 struct TweetDetailView: View {
     let tweet: Tweet
+    @ObservedObject var viewModel: TweetActionViewModel
+
+    init(tweet: Tweet) {
+        self.tweet = tweet
+        viewModel = TweetActionViewModel(tweet: tweet)
+    }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            HStack {
-                KFImage(URL(string: tweet.profileImageUrl))
-                    .resizable()
-                    .scaledToFill()
-                    .clipped()
-                    .frame(width: 56, height: 56)
-                    .cornerRadius(28)
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text(tweet.fullname)
-                        .font(.system(size: 14, weight: .semibold))
+        NavigationView {
+            VStack(alignment: .leading, spacing: 16) {
+                HStack {
+                    KFImage(URL(string: tweet.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .clipped()
+                        .frame(width: 56, height: 56)
+                        .cornerRadius(28)
                     
-                    Text("@\(tweet.username)")
-                        .font(.system(size: 14))
-                        .foregroundColor(.gray)
-                }
-            }
-            
-            Text(tweet.caption)
-                .font(.system(size: 22))
-            
-            Text(tweet.detailedTimestampString)
-                .font(.system(size: 14))
-                .foregroundColor(.gray)
-            
-            Divider()
-            
-            HStack(spacing: 12) {
-                HStack(spacing: 4) {
-                    Text("0")
-                        .font(.system(size: 14, weight: .semibold))
-                    
-                    Text("Retweets")
-                        .font(.system(size: 14))
-                        .foregroundColor(.gray)
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(tweet.fullname)
+                            .font(.system(size: 14, weight: .semibold))
+                        
+                        Text("@\(tweet.username)")
+                            .font(.system(size: 14))
+                            .foregroundColor(.gray)
+                    }
                 }
                 
-                HStack(spacing: 4) {
-                    Text("\(tweet.likes)")
-                        .font(.system(size: 14, weight: .semibold))
+                Text(tweet.caption)
+                    .font(.system(size: 22))
+                
+                Text(tweet.detailedTimestampString)
+                    .font(.system(size: 14))
+                    .foregroundColor(.gray)
+                
+                Divider()
+                
+                HStack(spacing: 12) {
+                    HStack(spacing: 4) {
+                        Text("0")
+                            .font(.system(size: 14, weight: .semibold))
+                        
+                        Text("Retweets")
+                            .font(.system(size: 14))
+                            .foregroundColor(.gray)
+                    }
                     
-                    Text("Likes")
-                        .font(.system(size: 14))
-                        .foregroundColor(.gray)
+                    HStack(spacing: 4) {
+                        Text("\(tweet.likes)")
+                            .font(.system(size: 14, weight: .semibold))
+                        
+                        Text("Likes")
+                            .font(.system(size: 14))
+                            .foregroundColor(.gray)
+                    }
                 }
+                
+                Divider()
+                
+                TweetActionsView(tweet: tweet)
+                
+                Spacer()
             }
-            
-            Divider()
-            
-            TweetActionsView(tweet: tweet)
-            
-            Spacer()
+            .padding()
         }
-        .padding()
+        .navigationBarItems(leading: Button(action: {
+            viewModel.checkIfUserLikedTweet()
+        }, label: {}))
     }
 }
